b93e8244d77bafeb3dab2dbd6311f993a1979c38
Drwalin <garekmarek@wp.pl>
2021-05-18 22:57:12 +0200

    WhichDicesToReroll done.

--- src/main/java/kosci/si/Probabilities.java
+++ src/main/java/kosci/si/Probabilities.java
@@ -1,34 +1,53 @@
 package kosci.si;
 
-// Assumes sorted arguments
+// Assumes arguments are sorted
+// dst can have variable length (ex.: three ones of a kind passes {1,1,1})
 
 public class Probabilities {
 
-	private class RerollDicesStruct {
-		public boolean[] reroll;
+	private static class RerollDicesStruct {
+		public RerollDicesStruct(int[] dst) {
+			missing = new boolean[dst.length];
+		}
+		public boolean[] reroll = new boolean[5];
 		public boolean[] missing;
+		public int missingCount = 0;
+		public int rerollCount = 0;
 	}
 
 	public static float ProbabilityOfHandFrom(int[] dices, int[] dst) {
-		boolean[] reroll = WhichDicesToReroll(dices, dst);
+		RerollDicesStruct reroll = WhichDicesToReroll(dices, dst);
 		return ProbabilityOfRethrowingDices(dices, reroll, dst);
 	}
 
-	public static boolean[] WhichDicesToReroll(int[] dices, int[] dst) {
-		boolean[] reroll = new boolean[] {true, true, true, true, true};
+	public static RerollDicesStruct WhichDicesToReroll(int[] dices, int[] dst) {
+		RerollDicesStruct ret = new RerollDicesStruct(dst);
 		int it = 0;
-		for(int dice : dst) {
-			for(; dices[it] < dice; ++it);
-			if(dices[it] == dice) {
-				reroll[it] = false;
-				++it;
+		for(int i=0; i<dst.length; ++i) {
+			for(; dices[it] < dst[i] && it<dices.length; ++it, ++ret.rerollCount)
+				ret.reroll[it] = true;
+			if(it < dices.length) {
+				if(dices[it] == dst[i]) {
+					ret.reroll[it] = false;
+					ret.missing[i] = false;
+					++it;
+				} else {
+					ret.reroll[it] = true;
+					ret.missing[i] = true;
+					++ret.missingCount;
+				}
+			} else {
+				ret.missing[i] = true;
+				++ret.missingCount;
 			}
 		}
-		return reroll;
+		return ret;
 	}
 
 	public static float ProbabilityOfRethrowingDices(int[] dices,
-		boolean[] reroll, int[] dst) {
+		RerollDicesStruct reroll, int[] dst) {
+
+
 		return 0.0f;
 	}
 
