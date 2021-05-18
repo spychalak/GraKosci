d431bafc271cd5857cfc44f2c3a76d8db7d8805e
Drwalin <garekmarek@wp.pl>
2021-05-18 21:06:38 +0200

    Done WhichDicesToReroll()

--- src/main/java/kosci/si/Probabilities.java
+++ src/main/java/kosci/si/Probabilities.java
@@ -4,22 +4,32 @@ package kosci.si;
 
 public class Probabilities {
 
-	public static float ProbabilityOfHandFrom(int[] dices, int[] dst) {
-		int dicesToRoll = 0;
-		for(boolean b : WhichDicesToReroll(dices, dst)) {
-			if(b)
-				dicesToRoll++;
-		}
+	private class RerollDicesStruct {
+		public boolean[] reroll;
+		public boolean[] missing;
+	}
 
+	public static float ProbabilityOfHandFrom(int[] dices, int[] dst) {
+		boolean[] reroll = WhichDicesToReroll(dices, dst);
+		return ProbabilityOfRethrowingDices(dices, reroll, dst);
 	}
 
 	public static boolean[] WhichDicesToReroll(int[] dices, int[] dst) {
-
+		boolean[] reroll = new boolean[] {true, true, true, true, true};
+		int it = 0;
+		for(int dice : dst) {
+			for(; dices[it] < dice; ++it);
+			if(dices[it] == dice) {
+				reroll[it] = false;
+				++it;
+			}
+		}
+		return reroll;
 	}
 
 	public static float ProbabilityOfRethrowingDices(int[] dices,
 		boolean[] reroll, int[] dst) {
-
+		return 0.0f;
 	}
 
 
